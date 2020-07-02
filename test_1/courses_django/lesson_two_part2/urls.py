917c14505cdba5fc6572ecc9aefc2aa5582c37e6
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-07-02 19:04:29 +0300

    improved urls-conf by added one prefix path

--- test_1/courses_django/lesson_two_part2/urls.py
+++ test_1/courses_django/lesson_two_part2/urls.py
@@ -2,16 +2,31 @@ from django.urls import path, re_path, include
 from . import views
 
 
-extra_patterns = [
-    re_path(r'^report/$', views.report),
-    re_path(r'^report/(?P<id>[0-9]+)/$', views.report),
-]
+# extra_patterns = [
+#     re_path(r'^report/$', views.report),
+#     re_path(r'^report/(?P<id>[0-9]+)/$', views.report),
+# ]
+#
+#
+# urlpatterns = [
+#     re_path(r'blog/(page-(\d+)/)?$', views.blog_articles),
+#     re_path(r'comments/(?:page-(?P<page_number>\d+)/)?$', views.comments),
+#     re_path(r'^optional-args/(?P<year>[0-9]{4})/$', views.optional_args, {'foo':'bar'}),
+#     re_path(r'extra/', include(extra_patterns)),
+# ]
 
+# urlpatterns = [
+#     re_path(r'^(?P<page_slug>[\w]+)-(?P<page_id>[\w]+)/history/$', views.history),
+#     path('<page_slug>-<page_id>/edit/', views.edit),
+#     path('<page_slug>-<page_id>/discuss/', views.discuss),
+#     path('<page_slug>-<page_id>/permissions/', views.permissions),
+# ]
 
 urlpatterns = [
-    re_path(r'blog/(page-(\d+)/)?$', views.blog_articles),
-    re_path(r'comments/(?:page-(?P<page_number>\d+)/)?$', views.comments),
-    re_path(r'^optional-args/(?P<year>[0-9]{4})/$', views.optional_args, {'foo':'bar'}),
-    re_path(r'extra/', include(extra_patterns)),
+    path('<page_slug>-<page_id>/', include([
+        path('history/', views.history),
+        path('edit/', views.edit),
+        path('discuss/', views.discuss),
+        path('permissions/', views.permissions),
+    ])),
 ]
-
