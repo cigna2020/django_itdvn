866e9aaf4de2e6335b48627e1ad8349cff12b1e3
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-07-08 19:39:12 +0300

    created models Author1 and Article

--- test_1/courses_django/lesson_fifth/models.py
+++ test_1/courses_django/lesson_fifth/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Author1(models.Model):
+
+    CHOICES_FOR_CITY = {
+        ('kyiv', 'Киев'),
+        ('chernigov', 'Чернигов'),
+        ('odessa', 'Одесса'),
+        ('lvov', 'Львов'),
+    }
+    name    = models.CharField(max_length=200, verbose_name='Имья автора',)
+    surname = models.CharField(max_length=200, verbose_name='Фамилия автора')
+    city    = models.CharField(choices=CHOICES_FOR_CITY, max_length=200, verbose_name='Город', help_text='Выбирите город со списка')
+
+    def __str__(self):
+        return 'Имя %s' % self.name
+
+class Article(models.Model):
+    author  = models.ForeignKey(Author1, verbose_name='Автор статьи')
+    title   = models.CharField(max_length=100, verbose_name='Заголовок')
+    text    = models.TextField(max_length=500, verbose_name='Текст статьи')
+
+    def __str__(self):
+        return self.title
+
+# сделать миграцию !!!
