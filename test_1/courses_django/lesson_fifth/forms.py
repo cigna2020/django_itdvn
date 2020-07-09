316e625a71f2d24a8f19d2f8b975a9b8063f92c4
cigna2020 <65529690+cigna2020@users.noreply.github.com>
2020-07-09 20:34:22 +0300

    added exceptions to the validation form

--- test_1/courses_django/lesson_fifth/forms.py
+++ test_1/courses_django/lesson_fifth/forms.py
@@ -1,4 +1,4 @@
-from django.core.validators import URLValidator
+from django.core.validators import URLValidator, ValidationError
 from django.forms import ModelForm, Form
 from . import models
 from django import forms
@@ -25,19 +25,44 @@ class ContactForm(forms.Form):
     sender = forms.EmailField(label='Введите Ваш имейл!')
 
 
+def validate_url(value):
+    validation_url = URLValidator()
+    value_one_invalid = False
+    value_two_invalid = False
+    try:
+        validation_url(value)
+    except:
+        value_one_invalid = True
+
+    value_two_url = 'http://' + value
+    try:
+        validate_url(value_two_url)
+    except:
+        value_two_invalid = True
+    if value_one_invalid == False and value_two_invalid == False:
+        raise forms.ValidationError('Неправильный адрес сайта!')
+    return value
+
+def check_dot_com(value):
+    if not '.com' in value:
+        raise ValidationError('Это не адрес сайта!')
+
+def check_itvdn(value):
+    if not 'itvdn' in value:
+        raise ValidationError('Это не сайт ИТВДН!')
+
 class UrlForm(forms.Form):
     title = forms.CharField(label='Название сайта')
-    url = forms.CharField(label="Адрес сайта")
+    url = forms.CharField(label="Адрес сайта", validators=[validate_url, check_dot_com, check_itvdn])
 
-    def clean(self):
-        clean_data = super(UrlForm, self).clean()
-
-    URLValidator
-    def clean_url(self):
-        url = self.cleaned_data['url']
-        validation_url = URLValidator()
-        try:
-            validation_url(url)
-        except:
-            raise forms.ValidationError('Это не адрес сайта!')
-        return url
+    # def clean(self):
+    #     clean_data = super(UrlForm, self).clean()
+    #
+    # def clean_url(self):
+    #     url = self.cleaned_data['url']
+    #     validation_url = URLValidator()
+    #     try:
+    #         validation_url(url)
+    #     except:
+    #         raise forms.ValidationError('Это не адрес сайта!')
+    #     return url
--- test_1/courses_django/lesson_fifth/urls.py
+++ test_1/courses_django/lesson_fifth/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     re_path(r'^search/$', views.search),
     re_path(r'^add-article/$', views.add_article),
     re_path(r'^add-author/$', views.author_add),
-    re_path(r'^url-form$', views.UrlView.as_view()),
+    re_path(r'^url-form/$', views.UrlView.as_view()),
 ]
